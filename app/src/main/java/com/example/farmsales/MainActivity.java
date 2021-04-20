0ea30f2b522bf5ef9029c2a3ac59d47ab76ef289
Maheshwaran R <maheshwaran.developer@gmail.com>
2021-04-20 15:12:47 +0530

    message

--- app/src/main/java/com/example/farmsales/MainActivity.java
+++ app/src/main/java/com/example/farmsales/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.farmsales;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.hbb20.CountryCodePicker;
+
+import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edtPhone;
 
+    public static EditText otp_et;
+    public AlertDialog alertDialog;
     private CountryCodePicker country_code;
+    private android.app.AlertDialog progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,37 +49,53 @@ public class MainActivity extends AppCompatActivity {
 
             else{
                 String Phone_number = "+" + country_code.getSelectedCountryCode() + edtPhone.getText().toString();
-                Verify_otp otp_verification = new Verify_otp();
-                otp_verification.sendVerificationCode(Phone_number);
-                buildDialog(otp_verification.otp);
+                buildDialog(Phone_number);
             }
         });
     }
 
-    private void buildDialog(String otp){
+    private void buildDialog(String phone){
+        progressBar = new SpotsDialog.Builder().setContext(MainActivity.this)
+                .setCancelable(false)
+                .setMessage("Auto-Retrieving OTP")
+                .setTheme(R.style.Custom)
+                .build();
+        progressBar.setCancelable(false);
+        progressBar.setCanceledOnTouchOutside(false);
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         View mView = getLayoutInflater().inflate(R.layout.activity_verify_otp,null);
-        final EditText otp_et = mView.findViewById(R.id.enter_otp);
+        otp_et = mView.findViewById(R.id.enter_otp);
         Button verify_bt = mView.findViewById(R.id.verify_otp);
         Button resend_bt = mView.findViewById(R.id.resend_otp);
         builder.setView(mView);
-        AlertDialog alertDialog = builder.create();
+        alertDialog = builder.create();
+        Context context = this;
+        Verify_otp otp_verification = new Verify_otp(otp_et,context,progressBar);
+        otp_verification.sendVerificationCode(phone);
         alertDialog.setCanceledOnTouchOutside(false);
-        if(!otp.isEmpty()){
-            otp_et.setText(otp);
-        }
+
         verify_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String otp = otp_et.getText().toString();
+                otp_verification.verifyCode(otp);
             }
         });
+
         resend_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                otp_verification.resendVerificationCode(phone, otp_verification.token);
             }
         });
         alertDialog.show();
+        progressBar.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        progressBar.dismiss();
+        alertDialog.dismiss();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/farmsales/Verify_otp.java
+++ app/src/main/java/com/example/farmsales/Verify_otp.java
@@ -3,6 +3,10 @@ package com.example.farmsales;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -23,6 +27,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
+import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
 
 import java.util.concurrent.TimeUnit;
@@ -32,12 +37,22 @@ public class Verify_otp extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private String verificationId;
-    public String otp;
-    public String message;
+    public String phone = "";
+    public EditText otp_et;
+    private android.app.AlertDialog progressDialog;
+    public Context context;
+    public PhoneAuthProvider.ForceResendingToken token = null;
+
+    public Verify_otp(EditText otp_et, Context context, android.app.AlertDialog progressDialog){
+        this.otp_et = otp_et;
+        this.context = context;
+        this.progressDialog = progressDialog;
+    }
 
     public void sendVerificationCode(String number) {
         // this method is used for getting
         // OTP on user phone number.
+        phone = number;
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
                 number,
                 60,
@@ -45,8 +60,19 @@ public class Verify_otp extends AppCompatActivity {
                 this,
                 mCallBack
         );
+        System.out.println(number);
     }
 
+    public void resendVerificationCode(String phoneNumber,
+                                        PhoneAuthProvider.ForceResendingToken token) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallBack,         // OnVerificationStateChangedCallbacks
+                token);             // ForceResendingToken from callbacks
+    }
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
@@ -56,8 +82,15 @@ public class Verify_otp extends AppCompatActivity {
         public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
             super.onCodeSent(s, forceResendingToken);
             verificationId = s;
+            token = forceResendingToken;
         }
 
+        @Override
+        public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+            progressDialog.dismiss();
+            Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+            super.onCodeAutoRetrievalTimeOut(s);
+        }
 
         @Override
         public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
@@ -65,23 +98,22 @@ public class Verify_otp extends AppCompatActivity {
             final String code = phoneAuthCredential.getSmsCode();
 
             if (code != null) {
-                otp = code;
-
                 verifyCode(code);
+                otp_et.setText(code);
             }
         }
 
 
         @Override
         public void onVerificationFailed(FirebaseException e) {
-            // displaying error message with firebase exception
-            message = e.getMessage();
+            progressDialog.dismiss();
+            Toast.makeText(context,e.getMessage(),Toast.LENGTH_SHORT).show();
         }
     };
 
 
 
-    private void verifyCode(String code) {
+    public void verifyCode(String code) {
         PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
         signInWithCredential(credential);
     }
@@ -90,18 +122,23 @@ public class Verify_otp extends AppCompatActivity {
 
     private void signInWithCredential(PhoneAuthCredential credential) {
 
+        mAuth = FirebaseAuth.getInstance();
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            message = "";
+                            Intent intent = new Intent(context,Home_page.class);
+                            context.startActivity(intent);
+                            ((Activity)context).finish();
                         }
 
                         else {
-                            message = task.getException().getMessage();
+                            progressDialog.dismiss();
+                            Toast.makeText(context,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                         }
                     }
+
                 });
     }
 
