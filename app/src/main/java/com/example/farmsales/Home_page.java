65063a0841f20d59480692d70f09870c9629a92f
Maheshwaran R <82704354+Mahi-developer@users.noreply.github.com>
2021-07-27 19:24:27 +0530

    updated list

--- app/src/main/java/com/example/farmsales/Home_page.java
+++ app/src/main/java/com/example/farmsales/Home_page.java
@@ -3,8 +3,11 @@ package com.example.farmsales;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,16 +23,22 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
+import org.jsoup.Jsoup;
+
+import java.util.ArrayList;
+
 public class Home_page extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private GoogleSignInClient mGoogleSignInClient;
+    private ArrayList<String> priceList = new ArrayList<String>();
+    private ListView priceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        new GetRates().execute();
         auth= FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -41,12 +50,19 @@ public class Home_page extends AppCompatActivity {
         if(user.getPhotoUrl()!=null){
             String photoUrl = user.getPhotoUrl().toString();
         }
+        priceView = findViewById(R.id.list);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finishAffinity();
+    }
+
+    public void setDataRates(){
+        System.out.println(priceList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,priceList);
+        priceView.setAdapter(adapter);
     }
 
     public void sign_out(){
@@ -58,5 +74,30 @@ public class Home_page extends AppCompatActivity {
                 finishAndRemoveTask();
             }
         });
+    }
+
+    private class GetRates extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                org.jsoup.nodes.Document doc = Jsoup.connect("https://rates.goldenchennai.com/vegetable-price/vellore-vegetable-price-today/").get();
+                org.jsoup.select.Elements rows = doc.select("tr");
+                for (org.jsoup.nodes.Element row : rows) {
+                    org.jsoup.select.Elements columns = row.select("td");
+                    for (org.jsoup.nodes.Element column : columns) {
+                        priceList.add(column.text());
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            setDataRates();
+        }
     }
 }
