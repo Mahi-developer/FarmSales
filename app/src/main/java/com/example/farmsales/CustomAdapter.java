576d30c03a9f2338b845f5b17058b6b13a1835cb
Maheshwaran R <82704354+Mahi-developer@users.noreply.github.com>
2021-07-28 09:25:21 +0530

    updated list_Image

--- app/src/main/java/com/example/farmsales/CustomAdapter.java
+++ app/src/main/java/com/example/farmsales/CustomAdapter.java
@@ -7,6 +7,16 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -48,9 +58,31 @@ public class CustomAdapter extends BaseAdapter{
         TextView name = (TextView) view.findViewById(R.id.product_name);
         TextView quantity = (TextView) view.findViewById(R.id.product_quantity);
         TextView price = (TextView) view.findViewById(R.id.product_price);
+        ImageView image_view = (ImageView)view.findViewById(R.id.product_image);
+        getData(image_view,product_name.get(i));
         name.setText(product_name.get(i));
         quantity.setText(product_quantity.get(i));
         price.setText(product_price.get(i));
         return view;
     }
+
+    public void getData(ImageView image_view,String product){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        DatabaseReference getImage = databaseReference.child(product);
+
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String link = dataSnapshot.getValue(String.class);
+                Picasso.get().load(link).into(image_view);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
